7a11d1884d9053ed2cf568d5cc1c45788019e434
Adam Gardner <adga2012@gmail.com>
2022-06-12 12:58:28 -0600

    chore: add profile module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, AdminModule],
+  imports: [PrismaModule, AuthModule, AdminModule, ProfileModule],
 })
 export class AppModule {}
