25e6d51a656e2fc97dfc2f583d6310e893ee7140
Adam Gardner <adga2012@gmail.com>
2022-06-16 22:01:15 -0600

    chore: add user roles table

---
+++ prisma/migrations/20220617033447_add_user_roles/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "role";
+
+-- CreateTable
+CREATE TABLE "user_roles" (
+    "user_id" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "user_roles_pkey" PRIMARY KEY ("user_id","role")
+);
+
+-- AddForeignKey
+ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
